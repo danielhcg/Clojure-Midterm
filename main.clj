2ff54ee4f4169d934f0333ac9eb710f484f04365
danielhcg <35074368+danielhcg@users.noreply.github.com>
2023-04-03 23:59:11 +0000

    created the arithmetic functions

--- main.clj
+++ main.clj
@@ -1,1 +1,18 @@
+; this is a function that takes in 2 arguments and calculates their sum 
+(defn add [x y]
+  (+ x y))
+
+; this is a function that takes in 2 arguemtns and calculates their difference
+(defn subtract [x y]
+  (- x y))
+
+; this is a function that takes in 2 arguments and calculates their product
+(defn multiply [x y]
+  (* x y))
+
+; this is a function that takes in 2 arguemtns and calculates their quotient
+(defn divide [x y]
+  (/ x y))
+
+
 (println "Hello World!")
