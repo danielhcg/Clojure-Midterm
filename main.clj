2b1dab4b8469767bd404fa6bcf85c49353a6e543
danielhcg <35074368+danielhcg@users.noreply.github.com>
2023-04-04 05:10:01 +0000

    completed everything left on the list for the assignemtn

--- main.clj
+++ main.clj
@@ -2,28 +2,11 @@
 (defn add [x y]
   (+ x y))
 
-; this is a function that takes in 2 arguemtns and calculates their difference
-(defn subtract [x y]
-  (- x y))
-
-; this is a function that takes in 2 arguments and calculates their product
-(defn multiply [x y]
-  (* x y))
-
-; this is a function that takes in 2 arguemtns and calculates their quotient
-(defn divide [x y]
-  (/ x y))
-
 ; string declaration, assignment, concatentation, and printing
-(def titleMessage "\nWelcome to Daniel's Clojure Calculator App")
-(println titleMessage)
-(def prompt1 "\nSelect an operation ")
-(def prompt2 "to perfrom (+, -, *, /)")
-(def prompt (str prompt1 prompt2))
-(println prompt)
-
-; declaring a variable called useOP and assigning it character by extracting first character of user inputted string 
-(def useOP (subs (read-line) 0 1))
+(def title1 "\nWelcome to Daniel's ")
+(def title2 "Clojure Arithmetic App\n")
+(def title (str title1 title2))
+(println title)
 
 ; prompting user for first operand, returns value 1 
 (defn val1 []
@@ -37,22 +20,49 @@
   (flush); to flush out buffer
   (Double/parseDouble (read-line))) 
 
+; for loop char array
+(def sumArray "Sum")
+
+; function that calls multi param function to calculate sum 
 (defn calcSum []
   (let [x (add (val1) (val2))]
-    (println "sum = " x)))
+    (doseq [i sumArray]
+      (print i))
+    (println " " x "\n")))
 
 (calcSum)
 
+(print "Printing values using a for loop: ")
+; for loop prints all values between 4 and 20 and adds it with itself at each iteration 
+(println (for [i (range 4 20)]
+           (+ i i)))
+
+println "\n"
+(print "Printing values using a while loop: ")
+; using a while loop to print numbers 1 -10
+(let [i (atom 1)] ; i is the atom that is initially set to 0
+  (while (< @i 11); while loop statement that iterates while less than 10
+    (print @i " ")
+    (swap! i inc))) ; increments i each iteration 
+(print "\nPrinting values using a dotime loop: ")
+(dotimes [i 10]
+  (print i " "))
+; using reduce to find the sum of all of these numbers starting at 0
+(println "\nPrinting the sum of 1 2 3 4 5: " (reduce + 0 [1 2 3 4 5]))
+
+(println "Printing 2 3 4 5 6 but one less using map" (map dec [2 3 4 5 6]))
+
+(println "\nPrinting only the odds from 1 2 3 4 5 6 7 8 using filter: " (filter odd? [1 2 3 4 5 6 7 8]))
 
 
 
-; for loop
-; while loop
-; doseq
-; dotimes
-; reduce
-; map
-; filter
+; for loop @
+; while loop @
+; doseq @
+; dotimes @
+; reduce @
+; map @
+; filter @
 ;----------------------------
 ; double declaration @
 ; double assignment @
