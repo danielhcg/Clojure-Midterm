dc74d04cd4086688e0c98c315e28cc4d37014a84
danielhcg <35074368+danielhcg@users.noreply.github.com>
2023-04-04 04:09:48 +0000

    char declaration, assignment, and printing

--- main.clj
+++ main.clj
@@ -15,13 +15,19 @@
   (/ x y))
 
 ; string declaration, assignment, concatentation, and printing
-(def titleMessage "Welcome to Daniel's Clojure Calculator App")
+(def titleMessage "\nWelcome to Daniel's Clojure Calculator App")
 (println titleMessage)
 (def prompt1 "\nSelect an operation ")
 (def prompt2 "to perfrom (+, -, *, /)")
 (def prompt (str prompt1 prompt2))
 (println prompt)
+(flush); to ensure prompt message precedes variable assignment 
 
+; declaring a variable called useOP and assigning it character by extracting first character of user inputted string 
+(let [useOP (subs (read-line) 0 1) ]
+  (println "The value of useOP is " useOP)) 
+
+; pr
 
 
 ; for loop
@@ -36,9 +42,9 @@
 ; double assignment 
 ; print double
 
-; char declaration 
-; char assignment
-; print char 
+; char declaration @
+; char assignment @
+; print char @
 
 ; print string @
 ;---------------------------
