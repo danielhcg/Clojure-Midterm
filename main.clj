add0ade16394e67d6106b604755117de1ce1bf86
danielhcg <35074368+danielhcg@users.noreply.github.com>
2023-04-04 03:56:47 +0000

    Implemented declaration, assignment, printing, and concatenation of a string

--- main.clj
+++ main.clj
@@ -14,5 +14,36 @@
 (defn divide [x y]
   (/ x y))
 
+; string declaration, assignment, concatentation, and printing
+(def titleMessage "Welcome to Daniel's Clojure Calculator App")
+(println titleMessage)
+(def prompt1 "\nSelect an operation ")
+(def prompt2 "to perfrom (+, -, *, /)")
+(def prompt (str prompt1 prompt2))
+(println prompt)
 
-(println "Hello World!")
+
+
+; for loop
+; while loop
+; doseq
+; dotimes
+; reduce
+; map
+; filter
+;----------------------------
+; double declaration 
+; double assignment 
+; print double
+
+; char declaration 
+; char assignment
+; print char 
+
+; print string @
+;---------------------------
+; string delcaration @
+; string assignment @
+; string concatenation @
+;---------------------------
+; multi parameter function @
